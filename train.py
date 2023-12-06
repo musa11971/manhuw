2862417cf35d640965a7da4fa365693dce30d5bb
Musa <musa@l400009.home>
2023-12-06 09:07:48 +0100

    wip

---
+++ train.py
@@ -0,0 +1,32 @@
+import librosa
+import numpy as np
+
+filename = 'audio/islam_sobhi/mulk.mp3'
+
+def extract_features(file_name):
+    # Loads the audio file as a floating point time series and assigns the default sample rate
+    # Sample rate is set to 22050 by default
+    X, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
+
+    # Generate Mel-frequency cepstral coefficients (MFCCs) from a time series
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+
+    # Generates a Short-time Fourier transform (STFT) to use in the chroma_stft
+    stft = np.abs(librosa.stft(X))
+
+    # Computes a chromagram from a waveform or power spectrogram.
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+
+    # Computes a mel-scaled spectrogram.
+    mel = np.mean(librosa.feature.melspectrogram(y=X, sr=sample_rate).T,axis=0)
+
+    # Computes spectral contrast
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+
+    # Computes the tonal centroid features (tonnetz)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),
+    sr=sample_rate).T,axis=0)
+
+    return mfccs, chroma, mel, contrast, tonnetz, filename
+
+print(extract_features(filename))
